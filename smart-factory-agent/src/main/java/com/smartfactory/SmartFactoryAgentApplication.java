08ffb893cfe06852f54af404c476097d0e6d4291
yuexihuachen <2578038405@qq.com>
2026-09-02 16:03:32 +0800

    chore: initialize Spring Boot project

---
+++ smart-factory-agent/src/main/java/com/smartfactory/SmartFactoryAgentApplication.java
@@ -0,0 +1,13 @@
+package com.smartfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFactoryAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFactoryAgentApplication.class, args);
+	}
+
+}
